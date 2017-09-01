1312548bfefd0c471be09a3c51e849f465ca73bd
Johannes Berdin <ich@johannesberdin.de>
2017-09-01 09:33:33 +0200

    Added configurable allowed sender list

--- drucker.php
+++ drucker.php
@@ -5,38 +5,59 @@ date_default_timezone_set('UTC');
 
 use PhpMimeMailParser\Parser;
 use Symfony\Component\Process\Process;
+use Symfony\Component\Yaml\Yaml;
 use Drucker\Drucker;
 use Drucker\Mail;
 
 require __DIR__.'/vendor/autoload.php';
 
-$mailPath = getcwd() . '/mails';
+$configPath = getcwd() . '/drucker.yml';
+$config = Yaml::parse(file_get_contents($configPath));
+
+$mailPath = getcwd() . DIRECTORY_SEPARATOR .  $config['mail path'];
 
 // Zee Drucker.
 $printer = new Drucker($mailPath);
 
 $mail = file_get_contents('php://stdin');
 
+$allowedSender = false;
+$checkSender = true;
+
 // Parse mail input.
 do {
     $parser = new Parser();
     $parser->setText($mail);
 
-    // Some nice logging.
-    echo "[inf] ".'Got mail: '.$parser->getHeader('from').' '.$parser->getHeader('subject') ."\n";
-
-    // Print attachments
-    $attachments = $parser->getAttachments();
-    foreach ($attachments as $attachment) {
-        $printer->queue($attachment->getContent());
+    // Checks if the sender is whitelisted for printing.
+    if ($checkSender) {
+        foreach ($config['allowed senders'] as $sender) {
+            if (preg_match("/$sender/", $parser->getHeader('from'))) {
+                $allowedSender = true;
+            }
+        }
     }
 
-    // Pipe parsed mail content to our quote extractor.
-    $parsedMail = new Mail($parser->getMessageBody('text'));
+    if ($allowedSender) {
+        $checkSender = false;
+        // Some nice logging.
+        echo "[inf] ".'Got mail: '.$parser->getHeader('from').' '.$parser->getHeader('subject') ."\n";
 
-    // Print the actual mail.
-    $printer->queue($parsedMail->getOriginal());
+        // Print attachments
+        $attachments = $parser->getAttachments();
+        foreach ($attachments as $attachment) {
+            //$printer->queue($attachment->getContent());
+        }
 
-    // Handle forwarded mail.
-    $mail = $parsedMail->getQuote();
+        // Pipe parsed mail content to our quote extractor.
+        $parsedMail = new Mail($parser->getMessageBody('text'));
+
+        // Print the actual mail.
+        //$printer->queue($parsedMail->getOriginal());
+
+        // Handle forwarded mail.
+        $mail = $parsedMail->getQuote();
+    } else {
+        $mail = null;
+    }
 } while (strlen($mail) > 0);
